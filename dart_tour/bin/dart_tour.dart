95b109189cecf7f0b1518cd91496e35db4b5baef
ddojai <heungyeol87@gmail.com>
2023-08-18 23:12:42 +0900

    1.0 Hello World

--- dart_tour/bin/dart_tour.dart
+++ dart_tour/bin/dart_tour.dart
@@ -1,5 +1,3 @@
-import 'package:dart_tour/dart_tour.dart' as dart_tour;
-
-void main(List<String> arguments) {
-  print('Hello world: ${dart_tour.calculate()}!');
+void main() {
+  print('Hello world!');
 }
