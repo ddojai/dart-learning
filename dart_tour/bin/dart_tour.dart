dc8784e040a1da5306df27b69b5936b233cad839
ddojai <heungyeol87@gmail.com>
2023-08-19 11:04:38 +0900

    2.3 collection for

--- dart_tour/bin/dart_tour.dart
+++ dart_tour/bin/dart_tour.dart
@@ -1,6 +1,8 @@
 void main() {
-  var name = 'maximin';
-  var age = 12;
-  var greeting = "Hello everyone, my name is $name and I'am ${age + 2}";
-  print(greeting);
+  var oldFriends = ['maximin', 'lacid'];
+  var newFriends = [
+    'lewis',
+    for (var friend in oldFriends) "😀 $friend",
+  ];
+  print(newFriends);
 }
